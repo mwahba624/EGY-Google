a98ca1c790d35c4e9d1e445a5279eea02e7ebb5b
Mona Lisa <wahbanous@gmail.com>
2025-01-08 18:30:30 -0500

    modified server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,7 +31,7 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
